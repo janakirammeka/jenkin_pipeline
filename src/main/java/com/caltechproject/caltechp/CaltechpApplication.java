066ca8ccd73223e0526f4deff076524688aafa5c
RAM Meka <ram.jmeka@gmail.com>
2021-03-31 17:46:28 -0700

    commit to push to github

---
+++ src/main/java/com/caltechproject/caltechp/CaltechpApplication.java
@@ -0,0 +1,13 @@
+package com.caltechproject.caltechp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaltechpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaltechpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caltechproject/caltechp/CaltechpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caltechproject.caltechp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaltechpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
